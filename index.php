2bfc83b36d24bf7e9852efc61c2ae82c67c88492
Pleclown <pleclown+github@gmail.com>
2017-06-15 15:12:48 +0200

    Create index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
+		<meta name=viewport content="width=device-width, initial-scale=1">
+		<title>Is It Time To Pack?</title>
+style type="text/css" media="screen"><!--
+* { box-sizing: border-box; }
+body {margin: 0; padding: 0; font-family: Verdana,Geneva,sans-serif;}
+#outer { position: absolute; display: table; width: 100%; height: 99%; margin: 0 auto; }
+#upper { display: table-row; width: 100%; height: 85%; }
+#lower { display: table-row; width: 100%; height: 15%; max-height: 90px; }
+#content { position: relative; display: table-cell; vertical-align: middle; text-align: center; }
+#footer { position: relative; display: table-cell; vertical-align: bottom; text-align: center; }
+.big { font-size: 36pt; }
+--></style>
+</head>
+
+	<body bgcolor="#ffffff">
+	<div id="outer">
+		<div id="upper">
+			<div id="content">
 <?php
 if (!empty($_GET)) {
 $answer=$_GET['answer'];
@@ -25,3 +47,7 @@ else {
 <?php
 }
 ?>
+			</div>		
+		</div>
+	</body>
+</html>
